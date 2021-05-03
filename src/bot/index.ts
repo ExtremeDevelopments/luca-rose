8eeacbe3db79b50e70731833c4b40c511dd55612
Extreme <najarvis05@gmail.com>
2021-05-02 21:01:17 -0500

    Status

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -9,6 +9,6 @@ const worker = new Worker({ database: config.MONGO })
 
 worker.loadMiddlewares(path.resolve(__dirname, './middlewares'))
 worker.commands.load(path.resolve(__dirname, './commands'))
-
+worker.setStatus('watching', 'Discord Bots', 'online')
 worker.commands
   .middleware(cooldownMiddleware())
