07088edbd9636bc2d8aeaa88078acaf20b274b88
Extreme <najarvis05@gmail.com>
2021-05-02 20:57:45 -0500

    Intents updates

--- src/index.ts
+++ src/index.ts
@@ -9,16 +9,16 @@ const master = new Master(resolve(__dirname, './bot/index.js'), {
   token: config.DISCORD_TOKEN,
   shards: 'auto',
   shardsPerCluster: 1,
-  intents: 32767,
+  intents: 32511,
   cache: {
     users: true,
     members: true
   },
-  log: (msg, cluster) => {
+  log: (msg: string, cluster: any) => {
     log(cluster, master.processes.reduce((a, c) => c.id.length > a ? c.id.length : a, 1), msg)
   }
 })
 
 master.start()
-  .then(() => {})
+  .then(() => { })
   .catch((err) => { throw err })
